d197b7127c2a7bc810cf2645737beefceb7702e2
McAuley, Frank <Frank.McAuley@alliancedata.com>
2020-01-22 15:32:06 -0500

    FM - adding hamcrest for unit testing and writting new tests for the StatisticsUtils

---
+++ app/src/test/java/com/example/android/architecture/blueprints/todoapp/statistics/StatisticsUtilsTest.kt
@@ -0,0 +1,40 @@
+package com.example.android.architecture.blueprints.todoapp.statistics
+
+import com.example.android.architecture.blueprints.todoapp.data.Task
+import org.hamcrest.CoreMatchers.`is`
+import org.junit.Assert.*
+import org.junit.Test
+
+class StatisticsUtilsTest {
+
+    @Test
+    fun getActiveAndCompleteStats_noComplete_returnHundredZero() {
+
+        // Create an active task
+        val tasks = listOf<Task>(
+                Task("title", "desc", isCompleted = false)
+        )
+
+        // Call your function
+        val result = getActiveAndCompletedStats(tasks)
+
+        // Check the result
+        assertThat(result.activeTasksPercent, `is`(100f))
+        assertThat(result.completedTasksPercent, `is`(0f))
+    }
+
+    @Test
+    fun getActiveAndCompleteStats_oneComplete_returnZeroHundred(){
+
+        // Create a task that is complete
+        val tasks = listOf<Task>(
+                Task("title", "desc", isCompleted = true))
+
+        // Call your function
+        val result = getActiveAndCompletedStats(tasks)
+
+        // Check the result
+        assertThat(result.completedTasksPercent, `is`(100f))
+        assertThat(result.activeTasksPercent, `is`(0f))
+    }
+}
